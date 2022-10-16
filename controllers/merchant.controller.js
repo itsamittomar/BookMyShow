aa3f16a45bbcd0861bb3ac78bde08348b7e561c0
itsamittomar <amitsingh10d@gmail.com>
2022-10-16 10:19:32 +0530

    Added Merchant Controller

--- controllers/merchant.controller.js
+++ controllers/merchant.controller.js
@@ -1,20 +1,20 @@
   
 const { request, response } = require("express");
 const validation = require('express-validator/check');
-const moviebiz = require('../biz/movie.biz');
-const usermodel = require("../models/user.model");
-
-console.log("Inside Movie controller");
+const merchantBiz = require('../biz/merchant.biz');
 
 
-class MovieControl {
+console.log("Inside Merchant controller");
+
+
+class MerchantControl {
     register(app){
-        app.route('/movie')
+        app.route('/merchant')
         .get(async (request,response,next) =>{
             try{
                 const error = validation(request)
                 if(!error.isEmpty()){
-                    const error = new Error("Movie Doesn't Exist, kindly Add !! ");
+                    const error = new Error("Invalid Merchant ");
                     error.statusCode= 422;
                     throw error;
                 }
@@ -27,23 +27,23 @@ class MovieControl {
                 throw err
             }
             const id = request.body.id;
-            const MovieBiz = new moviebiz();
-            const result = await MovieBiz.getMovieData(id)
+            const merchantbiz = new merchantBiz();
+            const result = await merchantbiz.getMerchantData(id)
             console.log(result)
-            return response.json(result,{message:"Movie Fetched Successfully!!"})
+            return response.json(result,{message:"Merchant details Fetched Successfully!!"})
 
         })
 
         .post(async (request, response, next)=>{
             try{
                 const reqBody = request.body;
-                const MovieBiz = new moviebiz();
-                const result = await MovieBiz.createMovieRecord(reqBody)
-                response.json({message: result},"Movie Added Successfully!!")
+                const merchantbiz = new merchantBiz();
+            const result = await merchantbiz.createMerchantRecord(reqBody)
+                response.json({message: result},"Merchant Added Successfully!!")
             }
             catch(err){
                 const error = new Error("Not able to add movie details")
-                error.message = "Not able to Add Movie",
+                error.message = "Not able to Add Merchant",
                 error.statusCode=422
                 throw error
                 //response.json({message:"Facing Issue"})
@@ -54,4 +54,4 @@ class MovieControl {
     }
 }
 
-module.exports=MovieControl;
+module.exports=MerchantControl;
